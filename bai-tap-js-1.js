664673535380a4f2cf3f21f0fd982f9e0b9b09b3
biencuong2112 <ptuan1296@gmail.com>
2020-07-22 10:35:28 +0700

    first commit

---
+++ bai-tap-js-1.js
@@ -0,0 +1,41 @@
+// Number
+ var a = 9;
+ var b = 10;
+
+//  string
+var text = 'My Name Tuan';
+
+// boolean ( true, flase) 
+var ismale = true;
+var isfemale = false;
+
+// undefined
+var a;
+console.log(a) => undefined
+
+// null
+???
+
+// object ( key: value,)
+var me = {
+ name: 'tuan',
+ length: '15cm',
+ age: 24,
+ gender: 'male'
+};
+console.log(me.name)
+
+// Array
+var motor1 = {
+    name: 'wave'   
+}
+
+var motor2 = {
+    name: 'sh'
+}
+
+var motor = [motor1,motor2]
+console.log(number);
+console.log(number.[0]) 
+console.log(number.[0].name) 
+
