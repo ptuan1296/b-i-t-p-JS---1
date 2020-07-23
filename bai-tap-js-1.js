e26d94003c51530c014fb76ce7cf359fb198c383
biencuong2112 <ptuan1296@gmail.com>
2020-07-23 09:25:20 +0700

    update bt 23/7

--- bai-tap-js-1.js
+++ bai-tap-js-1.js
@@ -2,8 +2,10 @@
  var a = 9;
  var b = 10;
 
-//  string
-var text = 'My Name Tuan';
+//  string ( có 3 cách khai báo str. 1:'', 2: "", 3:template
+C1: var text = 'My Name Tuan';
+C2: var text = "My Name Tuan";
+C3: var text = `My Name Tuan`;
 
 // boolean ( true, flase) 
 var ismale = true;
@@ -11,12 +13,12 @@ var isfemale = false;
 
 // undefined
 var a;
-console.log(a) => undefined
+console.log(a) => undefined ( vì khai báo biến a mà k gán giá trị gọi là undefined. k xác định ) 
 
 // null
-???
+var d = null ( rỗng )
 
-// object ( key: value,)
+// object ( key: value,) 
 var me = {
  name: 'tuan',
  length: '15cm',
@@ -25,7 +27,9 @@ var me = {
 };
 console.log(me.name)
 
-// Array
+// Array ( mảng có thể chứa 1: number, 2: object )
+VD1: var mobie = ['samsung', 'apple', 'oppo']
+VD2: var number = [1,3,5,7]
 var motor1 = {
     name: 'wave'   
 }
@@ -34,8 +38,8 @@ var motor2 = {
     name: 'sh'
 }
 
-var motor = [motor1,motor2]
-console.log(number);
-console.log(number.[0]) 
-console.log(number.[0].name) 
+var motor = [motor1,motor2] 
+console.log(motor);
+console.log(motor.[0])       ( lấy thành phần trong mảng theo thứ tự 0 1 2... ) kết quả: {name: 'wave'}
+console.log(motor.[1].name)  ( truy xuất vào thuộc tính của thành phần trong mảng ) kết quả:  sh
 
